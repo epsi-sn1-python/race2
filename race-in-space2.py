0dcfb66ecb1117b377a330e38f25f482bf54afe4
Selmac Depret <selmac.depret@epsi.fr>
2023-01-18 10:25:36 +0100

    start and return winner with sorted

--- race-in-space2.py
+++ race-in-space2.py
@@ -1,21 +1,36 @@
 class Track():
-    def __init__(self):
-        self._ship_list=[]
-    def add_ship(self):
-        self._ship_list.append(self)
+    def __init__(self,distance):
+        self._distance=distance
+        self._spaceship_list=[]
+        self._winner=None
+    def add_spaceship(self,spaceship):
+        self._spaceship_list.append(spaceship)
+    def start_race(self):
+        self._winner= sorted(self._spaceship_list,key=lambda item:item.get_vitesse())[-1:]
+    def get_winner(self):
+        return self._winner
 
-class Ship():
+class SpaceShip():
     def __init__(self,nom,vitesse):
         self._vitesse=vitesse
         self._nom=nom
-    def get_nom(self):
-        return self._nom
+    def get_vitesse(self):
+        return self._vitesse
+    def __repr__(self):
+        return f'{self._nom}:{self._vitesse} km/h'
 
-course1=Track()
-SShip1=Ship("baltazar1",15)
-SShip2=Ship("baltazar2",5)
-SShip3=Ship("baltazar3",2)
-SShip4=Ship("baltazar4",20)
-SShip5=Ship("baltazar5",8)
+track1=Track(2000)
+SpaceShip1=SpaceShip(5,"SpaceS1")
+SpaceShip2=SpaceShip(8,"SpaceS2")
+SpaceShip3=SpaceShip(6,"SpaceS3")
+SpaceShip4=SpaceShip(7,"SpaceS4")
+SpaceShip5=SpaceShip(3,"SpaceS5")
 
-course1.add_ship()
+track1.add_spaceship(SpaceShip1)
+track1.add_spaceship(SpaceShip2)
+track1.add_spaceship(SpaceShip3)
+track1.add_spaceship(SpaceShip4)
+track1.add_spaceship(SpaceShip5)
+
+track1.start_race()
+print(track1.get_winner())
